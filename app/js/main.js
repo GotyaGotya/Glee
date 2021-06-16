04ced8ad7586f1f79f37861f1d231aa5236b2e7f
GotyaGotya <mazamazaccc@gmail.com>
2021-06-16 17:35:11 +0300

    added desing gallery, connected mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,22 @@ $(function(){
     autoplay: true
   });
 
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+  // var mixer1 = mixitup('.product__items');
+  // var mixer2 = mixitup('.desing__items');
+
+
+
 });
