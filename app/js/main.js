f546675230c2c2f0beb7d9aeea0674344f0a1ec4
GotyaGotya <mazamazaccc@gmail.com>
2021-07-17 01:24:51 +0300

    add shop list page, fix menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,47 @@
 $(function(){
 
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function() {
+    $('.product-item').addClass('product-item--list')
+  })
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list')
+  })
+
+  $('.select-style').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade:   true,
     autoplay: true
   });
-
+  
+  $('.rate').rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "8px"
+  });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
   var containerEl2 = document.querySelector('[data-ref="container-2"]');
@@ -19,8 +54,6 @@ $(function(){
 
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
-
-
 
 
 
