f4a1ef4257d68cd7f3ed14c4cab6be10a30c0846
GotyaGotya <mazamazaccc@gmail.com>
2021-08-05 22:39:27 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,32 @@
 $(function(){
 
+  $('.product-tabs__top-item').on('click', function(e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+
+  $('.product-detalis-slide__thumb').slick({
+    asNavFor: '.product-detalis-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+    arrows: false,
+  });
+  $('.product-detalis-slide__big').slick({
+    asNavFor: '.product-detalis-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
+
   $('.shop-content__filter-btn').on('click', function() {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
@@ -13,7 +40,7 @@ $(function(){
     $('.product-item').removeClass('product-item--list')
   })
 
-  $('.select-style').styler();
+  $('.select-style, .product-detalis-item__num').styler();
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
@@ -26,6 +53,15 @@ $(function(){
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
+  });
+
+  
+
+  $('.product-related__slider-box').slick({
+    slidesToShow: 4,
+    dots: false,
+    autoplay: true,
+    appendArrows:$('.product-related__arrow'),
   });
 
   $('.top-slider__inner').slick({
